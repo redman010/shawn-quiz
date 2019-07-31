b8c10d85b0702f401b68a5814614efaad46924f2
shawn <51268110+redman010@users.noreply.github.com>
2019-07-31 18:52:13 -0400

    Update QAcommandindex.js
    changed some questions

--- QAcommandindex.js
+++ QAcommandindex.js
@@ -1,3 +1,127 @@
+
+const STORE = [
+  {
+    question: '1) Which of the following is correct about features of JavaScript?,
+    answers: [
+      'A) JavaScript is a lightweight, interpreted programming language.', 
+      'B) JavaScript is designed for creating network-centric applications.',
+      'C) JavaScript is complementary to and integrated with Java.',
+      'D) All of the above',
+      ],
+    correctAnswer: 'D) All of the above',
+    icon: 'https://i.imgur.com/G3IuGiC.jpg',
+    alt: 'ant lifting a branch saying you can do this'
+  },
+  {
+    question: '2) Which built-in method returns the character at the specified index?',
+    answers: [
+     ' A) characterAt()',
+      'B) getCharAt()',
+      'C) charAt()',
+      'D) None of the above.'
+      ],
+      correctAnswer: 'C) charAt()',
+      icon: 'https://live.staticflickr.com/3335/3293759838_12a7907fb1_m.jpg',
+      alt: 'odler man setting a computer with rooting growing from his feet'
+  },
+  {
+    question: 'Which built-in method returns the string representation of the number's value?',
+    answers: [
+      'A) None of the above.',
+      'B) toString()',
+      'C) toNumber()',
+      'D) toValue()',
+      ],
+    correctAnswer: 'C) toNumber()',
+    icon: 'https://i.imgur.com/FEZWyKl.jpg',
+    alt: 'duck tumbs up'
+   },
+   {
+      question: '4) what name should not be used for an AI program?',
+      answers: [
+          'A) undefine',
+          'B) humen',
+          'C) sky net',
+          'D) pet'
+      ],
+      correctAnswer: 'C) sky net',
+      icon: 'https://i.imgur.com/G3IuGiC.jpg',
+      alt: 'ant lifting a branch saying you can do this'
+    },
+  {
+    question: '5) Which of the following function of Boolean object returns a string of either 'true' or 'false' depending upon the value of the object?',
+    answers: [
+      'A) toSource()',
+      'B) valueOf()',
+      'C) toString()',
+      'D) None of the above.',
+      ],
+    correctAnswer: 'C) toString()',
+    icon: 'https://i.imgur.com/G3IuGiC.jpg',
+    alt: 'ant lifting a branch saying you can do this'
+  },
+  {
+    question: '6)Which of the following function of String object is used to match a regular expression against a string?',
+    answers: [      
+      'A) concat()',       
+      'B) match()',
+      'C) search()',
+      'D) replace()'
+      ],
+    correctAnswer: 'B) match()',
+    icon: 'https://live.staticflickr.com/1827/41674312120_36cb6dc371_n.jpg',
+    alt: 'kids climing steps'
+  },
+  {
+    question: '7) You are a Server Administrator at a small company. Company policy dictates that all remote connections must be encrypted and accessed using some form of authentication. Which of the following will allow a user to upload a large file from home, while adhering to company policy?',
+    answers: [
+          'A) SSH',
+          'B) SMTP',
+          'C) SFTP',
+          'D) FTP'
+    ],
+    correctAnswer: 'C) SFTP',
+    icon: 'https://i.imgur.com/VM8Y3MM.gif',
+    alt: 'matrix animation 1'
+  },
+  {
+    question: '8) A network analyst received a number of reports that impersonation was taking place on the network. Session tokens were deployed to mitigate this issue and defend against which of the following attacks?',
+    answers: [
+      'A) DDos',
+      'B) Phishing',
+      'C) Replay',
+      'D) Smurf'        
+    ],
+    correctAnswer: 'C) Replay',
+    icon: 'https://live.staticflickr.com/3618/3567877748_f3d35539e8_n.jpg',
+    alt: 'a person flexing'
+  },
+  {
+    question: '9) Which of the following is the MOST secure protocol to transfer files?',
+    answers: [
+      'A) FTP',
+      'B) TELNET',
+      'C) FTPS',
+      'D) SSH'
+    ],
+    correctAnswer: 'C) FTPS',
+    icon: 'https://live.staticflickr.com/65535/46947115935_6ae799066a_n.jpg',
+    alt: 'dominos lined up topping over to the last one'
+  },
+  {
+    question: '10) Which of the following should Bill, a security administrator, perform before a hard drive is analyzed with forensics tools?',
+    answers: [
+      'A) Disconnect system from network',
+      'B) Interview witnesses',
+      'C) Capture system image',
+      'D) Identify user habits'
+  ],
+  correctAnswer: 'C) Capture system image',
+
+  icon: 'https://live.staticflickr.com/8346/8206431705_ebb7e9f2f5_n.jpg',
+  alt: 'street sign saying end 44'
+  },
+];
 const otherSTORE = [
   {
     question: '1 How many 8 -bit character does the ASCII standard define',
@@ -48,12 +172,12 @@ const otherSTORE = [
     alt: 'ant lifting a branch saying you can do this'
   },
   {
-    question: '5 For linux Which tool is used to configure a sound card device?',
+    question: '5 Which of the following function of Array object applies a function simultaneously against two values of the array (from left-to-right) as to reduce it to a single value?',
     answers: [
-      'A. sndconf',
-      'B. sndconfig',
-      'C. soundconf',
-      'D. soundconfig'
+      'A. pop()',
+      'B. push()',
+      'C. reduce()',
+      'D. reduceRight()'
       ],
     correctAnswer: 'B. sndconfig',
     icon: 'https://i.imgur.com/G3IuGiC.jpg',
@@ -121,130 +245,6 @@ const otherSTORE = [
     alt: 'street sign saying end 22'
     }
   ];
-const STORE = [
-  {
-    question: '1) You are trying to use RDP to control a Windows computer from your office to an accountant computer in the corporate office building. What port should be opened in the firewalls between the two locations?',
-    answers: [
-      'A) 21', 
-      'B) 22',
-      'C) 3389',
-      'D) 143',
-      ],
-    correctAnswer: 'C) 3389',
-    icon: 'https://i.imgur.com/G3IuGiC.jpg',
-    alt: 'ant lifting a branch saying you can do this'
-  },
-  {
-    question: '2) Which of the following properly describes the purpose of a KVM?',
-    answers: [
-     ' A) Allows for touch screen interaction',
-      'B) Allows for one computer to be viewed on many monitors at once',
-      'C) Allows for one set of peripherals to control multiple computers',
-      'D) Enhanced graphics for video games'
-      ],
-      correctAnswer: 'C) Allows for one set of peripherals to control multiple computers',
-      icon: 'https://live.staticflickr.com/3335/3293759838_12a7907fb1_m.jpg',
-      alt: 'odler man setting a computer with rooting growing from his feet'
-  },
-  {
-    question: '3) Which of the following properly describes the purpose of a KVM?',
-    answers: [
-
-      'A) Allows for touch screen interaction',
-      'B) Allows for one computer to be viewed on many monitors at once',
-      'C) Allows for one set of peripherals to control multiple computers',
-      'D) Enhanced graphics for video games',
-      ],
-    correctAnswer: 'C) Allows for one set of peripherals to control multiple computers',
-    icon: 'https://i.imgur.com/FEZWyKl.jpg',
-    alt: 'duck tumbs up'
-   },
-   {
-      question: '4) what name should not be used for an AI program?',
-      answers: [
-          'A) undefine',
-          'B) humen',
-          'C) sky net',
-          'D) pet'
-      ],
-      correctAnswer: 'C) sky net',
-      icon: 'https://i.imgur.com/G3IuGiC.jpg',
-      alt: 'ant lifting a branch saying you can do this'
-    },
-  {
-    question: '5) Of the following choices, which is an industry standard protocol used by computer networks to assign IP Addresses to network nodes and devices?',
-    answers: [
-      'A) DNS',
-      'B) ARP',
-      'C) DHCP',
-      'D) RARP',
-      ],
-    correctAnswer: 'C) DHCP',
-    icon: 'https://i.imgur.com/G3IuGiC.jpg',
-    alt: 'ant lifting a branch saying you can do this'
-  },
-  {
-    question: '6) To identify TCP/IP errors such as connection problems ______ command can be used',
-    answers: [
-      'A) FTP',       
-      'B) POP3',
-      'C) Ping',
-      'D) STP'
-      ],
-    correctAnswer: 'C) Ping',
-    icon: 'https://live.staticflickr.com/1827/41674312120_36cb6dc371_n.jpg',
-    alt: 'kids climing steps'
-  },
-  {
-    question: '7) You are a Server Administrator at a small company. Company policy dictates that all remote connections must be encrypted and accessed using some form of authentication. Which of the following will allow a user to upload a large file from home, while adhering to company policy?',
-    answers: [
-          'A) SSH',
-          'B) SMTP',
-          'C) SFTP',
-          'D) FTP'
-    ],
-    correctAnswer: 'C) SFTP',
-    icon: 'https://i.imgur.com/VM8Y3MM.gif',
-    alt: 'matrix animation 1'
-  },
-  {
-    question: '8) A network analyst received a number of reports that impersonation was taking place on the network. Session tokens were deployed to mitigate this issue and defend against which of the following attacks?',
-    answers: [
-      'A) DDos',
-      'B) Phishing',
-      'C) Replay',
-      'D) Smurf'        
-    ],
-    correctAnswer: 'C) Replay',
-    icon: 'https://live.staticflickr.com/3618/3567877748_f3d35539e8_n.jpg',
-    alt: 'a person flexing'
-  },
-  {
-    question: '9) Which of the following is the MOST secure protocol to transfer files?',
-    answers: [
-      'A) FTP',
-      'B) TELNET',
-      'C) FTPS',
-      'D) SSH'
-    ],
-    correctAnswer: 'C) FTPS',
-    icon: 'https://live.staticflickr.com/65535/46947115935_6ae799066a_n.jpg',
-    alt: 'dominos lined up topping over to the last one'
-  },
-  {
-    question: '10) Which of the following should Bill, a security administrator, perform before a hard drive is analyzed with forensics tools?',
-    answers: [
-      'A) Disconnect system from network',
-      'B) Interview witnesses',
-      'C) Capture system image',
-      'D) Identify user habits'
-  ],
-  correctAnswer: 'C) Capture system image',
-
-  icon: 'https://live.staticflickr.com/8346/8206431705_ebb7e9f2f5_n.jpg',
-  alt: 'street sign saying end 44'
-  },
-];
 
 let myquestNumber = 0;
 var myScore = 0;
